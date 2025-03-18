95916fec328a45cf3750729252c64e2856889ecb
Marius Brehler <marius.brehler@amd.com>
2025-03-18 18:48:07 +0100

    Build hipBLASLt clients benchmarks (#217)
    Progress on #174.

--- math-libs/BLAS/post_hook_hipBLASLt.cmake
+++ math-libs/BLAS/post_hook_hipBLASLt.cmake
@@ -10,6 +10,13 @@ therock_set_install_rpath(
 
 therock_set_install_rpath(
   TARGETS
+    hipblaslt-bench
+    hipblaslt-bench-extop-amax
+    hipblaslt-bench-extop-layernorm
+    hipblaslt-bench-extop-matrixtransform
+    hipblaslt-bench-extop-softmax
+    hipblaslt-bench-groupedgemm-fixed-mk
+    hipblaslt-sequence
     hipblaslt-test
   PATHS
     ../lib
