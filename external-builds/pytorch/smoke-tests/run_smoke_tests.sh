c2186a94fb59f856910a65f5fed2682515fe7857
erman-gurses <99776114+erman-gurses@users.noreply.github.com>
2025-03-25 12:47:57 -0700

    Add Docker image verification (#185)
    This PR runs a set of smoke tests on the docker container using the
    docker image from
    [here](https://github.com/ROCm/TheRock/pkgs/container/therock_pytorch_dev_ubuntu_24_04_gfx942)
    Currently, the verification focuses on GFX942 target
    The future work will be adding the multiple targets as matrix.
    
    ---------
    
    Signed-off-by: erman-gurses <erman.gurses@amd.com>

---
+++ external-builds/pytorch/smoke-tests/run_smoke_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo 'Running inside the container'
+
+echo 'Set path to .local/bin'
+export PATH="$HOME/.local/bin:$PATH"
+
+echo 'Check Python version'
+python3 --version
+
+echo 'List the pip packages'
+pip list
+
+echo 'Install pytest'
+PIP_BREAK_SYSTEM_PACKAGES=1 pip install --no-index --find-links=/wheels pytest;
+echo 'Run smoke tests'
+pytest -v external-builds/pytorch/smoke-tests/pytorch_smoke_tests.py
+
+echo 'Task completed!'
