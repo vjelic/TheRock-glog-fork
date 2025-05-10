86c5bc28c3523c102c627cf90b80eae7c262e41a
Stella Laurenzo <slaurenz@amd.com>
2025-05-09 19:56:31 -0700

    Reapply "[windows] Generate versioned amd_comgr.dll. (#579)" (#592)
    This reverts commit 4b591962821f78ccac9badf7b9f3cbf8eeaf4a2b.
    
    Adds clr patch to use is_versioned consistently (upstreamed as
    https://github.com/ROCm/clr/pull/162).

--- compiler/pre_hook_amd-comgr.cmake
+++ compiler/pre_hook_amd-comgr.cmake
@@ -9,3 +9,36 @@ endif()
 set(BUILD_TESTING OFF CACHE BOOL "DISABLE BUILDING TESTS IN SUBPROJECTS" FORCE)
 
 set(CMAKE_INSTALL_RPATH "$ORIGIN;$ORIGIN/llvm/lib;$ORIGIN/rocm_sysdeps/lib")
+
+# See Comgr::LoadLib in clr comgrctx.cpp: On windows, this expects the cmgr
+# library to have a versioned output name, but there does not seem to be a
+# public patch to llvm-project/amd/comgr which sets it properly. Therefore,
+# we align it with what LoadLib expects:
+#   amd_comgrMMNN.dll
+# where MM is the left zero padded HIP_MAJOR_VERSION and NN is the left zero
+# padded HIP_MINOR_VERSION. There is no natural connection between these
+# projects in the codebase, so this is just an action at a distance / must
+# line up thing. We require the version explicitly from the caller and complain
+# loudly if not present.
+function(therock_patch_comgr_win_output_name)
+  if(NOT THEROCK_HIP_MAJOR_VERSION OR NOT THEROCK_HIP_MINOR_VERSION)
+    message(FATAL_ERROR "Super-project must set THEROCK_HIP_MAJOR_VERSION and THEROCK_HIP_MINOR_VERSION")
+  endif()
+  if(THEROCK_HIP_MAJOR_VERSION LESS_EQUAL 9)
+    set(THEROCK_HIP_MAJOR_VERSION "0${THEROCK_HIP_MAJOR_VERSION}")
+  endif()
+  if(THEROCK_HIP_MINOR_VERSION LESS_EQUAL 9)
+    set(THEROCK_HIP_MINOR_VERSION "0${THEROCK_HIP_MINOR_VERSION}")
+  endif()
+
+  set(suffix_version "${THEROCK_HIP_MAJOR_VERSION}${THEROCK_HIP_MINOR_VERSION}")
+  message(STATUS "Versioned comgr suffix: ${suffix_version}")
+
+  if(WIN32)
+    set(output_name "amd_comgr${suffix_version}")
+    message(STATUS "Override comgr output_name (windows): ${output_name}")
+    set_target_properties(amd_comgr PROPERTIES OUTPUT_NAME "${output_name}")
+  endif()
+endfunction()
+
+cmake_language(DEFER CALL therock_patch_comgr_win_output_name)
