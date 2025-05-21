cbce030201f1351cceff04c3e89882f0a6768f1f
geomin12 <geomin12@amd.com>
2025-05-21 16:50:16 -0700

    Adding script

--- build_tools/github_actions/monorepo_map.py
+++ build_tools/github_actions/monorepo_map.py
@@ -2,16 +2,8 @@
 This dictionary is used to map specific file directory changes to the corresponding build flag and tests
 """
 monorepo_map = {
-    "projects/hipcub": {
-        "flag": "-DTHEROCK_ENABLE_PRIM=ON -DTHEROCK_ENABLE_ALL=OFF",
-        "test": "test_hipcub"
-    },
     "projects/rocprim": {
-        "flag": "-DTHEROCK_ENABLE_PRIM=ON",
+        "flag": "-DTHEROCK_ENABLE_PRIM=ON -DTHEROCK_ENABLE_ALL=OFF",
         "test": "test_rocprim"
-    },
-    "projects/rocthrust": {
-        "flag": "-DTHEROCK_ENABLE_PRIM=ON",
-        "test": "test_rocthrust"
     }
 }
