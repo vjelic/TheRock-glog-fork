866f163742246445138d7addff8a4010e86bddee
Geo Min <geomin12@amd.com>
2025-06-04 14:42:12 -0700

    [monorepo] Moving `monorepo_configure_ci` to rocm-libraries (#740)
    Since configure_ci for monorepo is only used in monorepo, migrating
    those files over there

--- build_tools/github_actions/monorepo_map.py
+++
@@ -1,9 +0,0 @@
-"""
-This dictionary is used to map specific file directory changes to the corresponding build flag and tests
-"""
-monorepo_map = {
-    "projects/rocprim": {
-        "flag": "-DTHEROCK_ENABLE_PRIM=ON -DTHEROCK_ENABLE_ALL=OFF",
-        "test": "test_rocprim",
-    }
-}
