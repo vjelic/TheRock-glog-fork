1286fd46b6c052d37613e81bd99235f556e2cf6f
erman-gurses <erman.gurses@amd.com>
2025-06-23 20:34:18 -0700

    Seperate build from workflow

---
+++ external-builds/pytorch/pytorch_tests/build_pytorch_wheels.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -xeuo pipefail
+
+python_dir="/opt/python/${PYTHON_VERSION}"
+export PATH="$python_dir/bin:$PATH"
+
+cd /workspace/external-builds/pytorch/pytorch
+git am --abort || true
+git reset --hard
+git clean -xfd
+
+cd /workspace
+./external-builds/pytorch/pytorch_torch_repo.py checkout
+./external-builds/pytorch/pytorch_audio_repo.py checkout
+./external-builds/pytorch/pytorch_vision_repo.py checkout
+
+mkdir -p /tmp/pipcache
+./external-builds/pytorch/build_prod_wheels.py \
+  --pip-cache-dir /tmp/pipcache \
+  --index-url "https://${S3_CLOUDFRONT}/${S3_SUBDIR}/${AMDGPU_FAMILIES}/" \
+  build \
+  --install-rocm \
+  --clean \
+  --output-dir "$PACKAGE_DIST_DIR"
+
+python -m pip install "$PACKAGE_DIST_DIR"/torch-*.whl
+rocm-smi
+
+cd /workspace/external-builds/pytorch/pytorch
+python -m pip install pytest pytest-xdist
+
+export PYTORCH_TEST_WITH_ROCM=1
+set +e
+EXIT_CODE=0
+pytest test/test_nn.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_torch.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_cuda.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_ops.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_unary_ufuncs.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_binary_ufuncs.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest test/test_autograd.py -v --continue-on-collection-errors || EXIT_CODE=$?
+pytest torch/_inductor/test_torchinductor.py -v --continue-on-collection-errors || EXIT_CODE=$?
+exit $EXIT_CODE
