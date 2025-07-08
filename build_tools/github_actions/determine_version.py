348a01f86245feb49c68f452f3e8833ff7218186
Marius Brehler <marius.brehler@amd.com>
2025-07-08 08:48:01 +0200

    Explicitly set the ROCm version to use for building torch (#965)
    Instead of pulling the latest ROCm packages, this adds the option to
    pass the ROCm version that should be installed. This is also used to
    determine the local version identifier (which defaults to `rocmsdk` and
    the local date). Packages created with the modified workflow have the
    following version identier:
    
    * Nightly builds: torch-{torch base version}+rocm{base rocm
    version}rc{release date}
    * Dev builds: torch-{torch base version}+rocm{base rocm version}.dev{dev
    version}.{rocm commit sha}
    
    Examples for packages are:
    *
    torch-2.7.0a0+rocm7.0.0.dev0.515115ea2cb85a0b71b5507ce56a627d14c7ae73-cp312-cp312-linux_x86_64.whl
    * torch-2.7.0a0+rocm7.0.0rc20250702-cp312-cp312-linux_x86_64.whl
    
    Noticeably, the local version identifier starts with `rocm` and not with
    `rocmsdk` as in the default. ROCm dev builds themselves have a local
    version identifier making it necessary to replace the `+` as one would
    otherwise have two local version identifiers, resulting in an invalid
    version.
    
    The only downside of the proposed local version identifier is that the
    ones used for the dev packages result in a version that is evaluated
    larger as the ones of the nightly packages. With this, dev packages and
    nightly releases cannot be easily mixed and we might want to revise this
    later.

---
+++ build_tools/github_actions/determine_version.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""Determines the SDK version and version suffix to pass as additional
+arguments to `external-builds/pytorch/build_prod_wheels.py`.
+
+Example usage:
+
+    python determine_version.py --rocm-version 7.0.0 --write-env-file
+
+  The following string is appended to the file specified in the "GITHUB_ENV"
+  environment variable:
+
+    optional_build_prod_arguments=--rocm-sdk-version ==7.0.0 --version-suffix +rocm7.0.0
+
+Writing the output to the "GITHUB_ENV" file can be suppressed by passing
+`--no-write-env-file`.
+"""
+
+from packaging.version import Version, parse
+
+import argparse
+import os
+import sys
+
+
+def derive_versions(rocm_version: str, verbose_output: bool) -> str:
+    version = parse(rocm_version)
+    rocm_sdk_version = f"=={version}"
+    version_suffix = f"+rocm{str(version).replace('+','-')}"
+    optional_build_prod_arguments = (
+        f"--rocm-sdk-version {rocm_sdk_version} --version-suffix {version_suffix}"
+    )
+
+    if verbose_output:
+        print(f"ROCm version: {version}")
+        print(f"`--rocm-sdk-version`\t: {rocm_sdk_version}")
+        print(f"`--version-suffix`\t: {version_suffix}")
+        print()
+
+    return optional_build_prod_arguments
+
+
+def write_env_file(optional_build_prod_arguments: str):
+    env_file = os.getenv("GITHUB_ENV")
+
+    with open(env_file, "a") as f:
+        f.write(f"optional_build_prod_arguments={optional_build_prod_arguments}")
+
+
+def main(argv: list[str]):
+    p = argparse.ArgumentParser(prog="determine_version.py")
+    p.add_argument(
+        "--rocm-version",
+        required=True,
+        type=str,
+        help="ROCm version to derive the parameters from",
+    )
+    p.add_argument(
+        "--write-env-file",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Write `optional_build_prod_arguments` to GITHUB_ENV file",
+    )
+    p.add_argument(
+        "--verbose",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Verbose output",
+    )
+    args = p.parse_args(argv)
+
+    optional_build_prod_arguments = derive_versions(args.rocm_version, args.verbose)
+    print(f"{optional_build_prod_arguments}")
+
+    if args.write_env_file:
+        write_env_file(optional_build_prod_arguments)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
