9480a90c8d870a4316fdec2e479ee0283713fe31
geomin12 <geomin12@amd.com>
2025-07-22 15:27:42 -0700

    Adding fft

---
+++ build_tools/github_actions/test_executable_scripts/test_hipfft.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+
+logging.basicConfig(level=logging.INFO)
+
+cmd = [
+    f"{THEROCK_BIN_DIR}/hipfft-test",
+    "--gtest_filter=-*multi_gpu*",
+    "--precompile=rocfft-test-precompile.db",
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+subprocess.run(
+    cmd,
+    cwd=THEROCK_DIR,
+    check=True,
+)
