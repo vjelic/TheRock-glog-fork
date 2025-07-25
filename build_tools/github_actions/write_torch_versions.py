8e221281974fd5f251e29ed05d8f6746dbd813c1
arravikum <arravikum@amd.com>
2025-07-25 12:03:26 +0000

    Commenting out checkut of triton repo

--- build_tools/github_actions/write_torch_versions.py
+++ build_tools/github_actions/write_torch_versions.py
@@ -91,6 +91,7 @@ def get_all_wheel_versions(
 
 
 def main(argv: list[str]):
+<<<<<<< HEAD
     p = argparse.ArgumentParser(prog="write_torch_versions.py")
     p.add_argument(
         "--dist-dir",
@@ -104,6 +105,23 @@ def main(argv: list[str]):
     all_versions = get_all_wheel_versions(args.dist_dir)
     _log("")
     gha_set_output(all_versions)
+=======
+    # Get the torch version from the first torch wheel in PACKAGE_DIST_DIR.
+    package_dist_dir = os.getenv("PACKAGE_DIST_DIR")
+    torch_version = glob.glob("torch-*.whl", root_dir=package_dist_dir)[0].split("-")[1]
+    gha_set_output({"torch_version": torch_version})
+
+    torchaudio_version_tmp = glob.glob("torchaudio-*.whl", root_dir=package_dist_dir)
+    if torchaudio_version_tmp:
+        torchaudio_version = torchaudio_version_tmp[0].split("-")[1]
+        gha_set_output({"torchaudio_version": torchaudio_version})
+
+    torchvision_version_tmp = glob.glob("torchvision-*.whl", root_dir=package_dist_dir)
+    if torchvision_version_tmp:
+        torchvision_version = torchvision_version_tmp[0].split("-")[1]
+        gha_set_output({"torchaudio_version": torchaudio_version})
+
+>>>>>>> c41c84b (Commenting out checkut of triton repo)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
