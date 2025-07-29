fa36162824922259c216ddcd4b576f4f6341cb2d
geomin12 <geomin12@amd.com>
2025-07-29 16:32:11 -0700

    Adding new label

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -12,7 +12,7 @@ amdgpu_family_info_matrix_presubmit = {
     },
     "gfx110x": {
         "linux": {
-            "test-runs-on": "linux-rx7900-gpu-rocm",
+            "test-runs-on": "linux-gfx110X-gpu-rocm",
             "family": "gfx110X-dgpu",
             "pytorch-target": "gfx1100",
         },
