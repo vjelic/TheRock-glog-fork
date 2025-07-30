d088f7cc3f32f249edca95c9ec125a04824e3d4d
Marius Brehler <marius.brehler@amd.com>
2025-07-30 18:46:49 +0200

    Revise bump submodules helper (#1148)
    Revises the `bump_submodules` helper to allow bumping submodules
    belonging to a specific category / subdirectory. If `--components` isn't
    specified, `fetch_sources.py`'s default is used.

--- build_tools/bump_submodules.py
+++ build_tools/bump_submodules.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+"""Helper script to bump TheRock's submodules, doing the following:
+ * (Optional) Creates a new branch
+ * Updates submodules from remote using `fetch_sources.py`
+ * Creares a commit and tries to apply local patches
+ * (Optional) Pushed the new branch to origin
+
+The submodules to bump can be specified via `--components`.
+
+Examples:
+Bump submpdules in base, core and profiler
+```
+./build_tools/bump_submodules.py \
+    --components base core profiler
+```
+
+Bump comm-lib submodules and create a branch
+```
+./build_tools/bump_submodules.py \
+    --create-branch --branch-name shared/bump-comm-libs --components comm-libs
+```
+"""
 
 import argparse
 from pathlib import Path
@@ -52,7 +73,74 @@ def pin_ck():
     )
 
 
-def run(args):
+def parse_components(components: list[str]) -> list[list]:
+    arguments = []
+    system_projects = []
+
+    # If `default` is passed, use the defaults set in `fetch_sources.py` by not passing additonal arguments.
+    if "default" in components:
+        return [], []
+
+    if any(comp in components for comp in ["base", "comm-libs", "core", "profiler"]):
+        arguments.append("--include-system-projects")
+    else:
+        arguments.append("--no-include-system-projects")
+
+    if "base" in components:
+        system_projects += [
+            "half",
+            "rocm-cmake",
+            "rocm-core",
+            "rocm_smi_lib",
+            "rocprofiler-register",
+        ]
+
+    if "comm-libs" in components:
+        system_projects += [
+            "rccl",
+            "rccl-tests",
+        ]
+
+    if "core" in components:
+        # amdgpu-windows-interop is Windows only and is updated manually.
+        system_projects += [
+            "HIP",
+            "ROCR-Runtime",
+            "clr",
+            "rocminfo",
+        ]
+
+    if "profiler" in components:
+        system_projects += [
+            "aqlprofile",
+            "rocprof-trace-decoder",
+            "rocprofiler-sdk",
+            "roctracer",
+        ]
+
+    if "math-libs" in components:
+        arguments.append("--include-math-libs")
+    else:
+        arguments.append("--no-include-math-libs")
+
+    if "ml-libs" in components:
+        arguments.append("--include-ml-frameworks")
+    else:
+        arguments.append("--no-include-ml-frameworks")
+
+    if "compiler" in components:
+        arguments.append("--include-compilers")
+    else:
+        arguments.append("--no-include-compilers")
+
+    log(f"++ Arguments: {shlex.join(arguments)}")
+    if system_projects:
+        log(f"++ System projects: {shlex.join(system_projects)}")
+
+    return [arguments, system_projects]
+
+
+def run(args: argparse.Namespace, fetch_args: list[str], system_projects: list[str]):
     date = datetime.today().strftime("%Y%m%d")
 
     if args.create_branch or args.push_branch:
@@ -61,13 +149,20 @@ def run(args):
             cwd=THEROCK_DIR,
         )
 
+    if system_projects:
+        projects_args = ["--system-projects"] + system_projects
+    else:
+        projects_args = []
+
     exec(
         [
             sys.executable,
             "./build_tools/fetch_sources.py",
             "--remote",
             "--no-apply-patches",
-        ],
+        ]
+        + fetch_args
+        + projects_args,
         cwd=THEROCK_DIR,
     )
 
@@ -130,8 +225,25 @@ def main(argv):
         action=argparse.BooleanOptionalAction,
         help="Pin composable_kernel to version tagged in MIOpen",
     )
+    parser.add_argument(
+        "--components",
+        type=str,
+        nargs="+",
+        default="default",
+        help="""List of components (subdirectories) to bump. Choices:
+                  default,
+                  base,
+                  comm-libs,
+                  compiler,
+                  core,
+                  math-libs,
+                  ml-libs,
+                  profiler
+             """,
+    )
     args = parser.parse_args(argv)
-    run(args)
+    fetch_args, system_projects = parse_components(args.components)
+    run(args, fetch_args, system_projects)
 
 
 if __name__ == "__main__":
