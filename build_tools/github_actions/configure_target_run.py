21439d057aa3329ca3186f4a32f30ebd2fcc7476
Scott Todd <scott.todd0@gmail.com>
2025-08-06 16:42:43 -0700

    [ci] Rework test_runs_on plumbing for release workflows. (#1100)
    Progress on https://github.com/ROCm/TheRock/issues/589 and
    https://github.com/ROCm/TheRock/issues/1097.
    
    This changes `configure_target_run.py` to look for the target family in
    either the "inner family" or the "outer key", so it correctly chooses
    runners for gfx1151 instead of skipping that test configuration since
    `gfx115X` and `gfx1151` do not match. I also considered explicit data
    flow of `test_runs_on` through the workflows, but we preferred to keep
    some automatic detection so developers triggering workflows do not need
    to manually line up test families with test runners.
    
    Recent test runs:
    * "Build Windows PyTorch Wheels" with default inputs and ROCm version
    `7.0.0rc20250804`:
    https://github.com/ROCm/TheRock/actions/runs/16787740857
    * "Build Portable Linux PyTorch Wheels" with default inputs and ROCm
    version `7.0.0rc20250805`:
    https://github.com/ROCm/TheRock/actions/runs/16787747432
    * "Build Portable Linux PyTorch Wheels" with gfx110X-dgpu and ROCm
    version `7.0.0rc20250805`:
    https://github.com/ROCm/TheRock/actions/runs/16787789191

--- build_tools/github_actions/configure_target_run.py
+++ build_tools/github_actions/configure_target_run.py
@@ -1,3 +1,11 @@
+"""This file looks up the test-runs-on runner label for a given GPU family.
+
+Environment variable inputs:
+    * 'TARGET': A GPU family like 'gfx95X-dcgpu' or 'gfx1151', corresponding
+                to a release index.
+    * 'PLATFORM': "linux" or "windows"
+"""
+
 import os
 from amdgpu_family_matrix import (
     amdgpu_family_info_matrix_presubmit,
@@ -6,32 +14,46 @@ from amdgpu_family_matrix import (
 
 from github_actions_utils import *
 
-# This file helps configure which target to run
 
-# TODO (geomin12): this is very hard-coded to a very specific use-case.
-# Once portable_linux_package_matrix.yml matures, this will mature as well
-# Some logic is duplicated with fetch_package_targets.py
-
-
-def main(target: str, platform: str):
+def get_runner_label(target: str, platform: str) -> str:
+    print(f"Searching for a runner for target '{target}' on platform '{platform}'")
     amdgpu_family_info_matrix = (
         amdgpu_family_info_matrix_presubmit | amdgpu_family_info_matrix_postsubmit
     )
     for key, info_for_key in amdgpu_family_info_matrix.items():
-        # Only consider items containing the amdgpu_family (ex: gfx94X in gfx94X-dcgpu)
-        if key not in target.lower():
-            continue
-
+        print(f"Cheecking key '{key}' with info:\n  {info_for_key}")
         platform_for_key = info_for_key.get(platform)
 
         if not platform_for_key:
             # Some AMDGPU families are only supported on certain platforms.
+            print(f"  Skipping since this entry has no platform '{platform}'")
+            continue
+
+        # Check against both the inner "family" and the outer "key". If neither
+        # match then skip. Workflows are expected to use the inner "family"
+        # but manually triggered runs may use the outer "key" instead, so we'll
+        # be a bit lenient here.
+        # This needs a rework, see https://github.com/ROCm/TheRock/issues/1097.
+        family_for_platform = platform_for_key.get("family")
+        if target != family_for_platform and key not in target.lower():
+            print(
+                f"  Skipping since the target '{target}' does not match the family '{family_for_platform}'"
+            )
             continue
 
         # If there is a test machine available for this target, run on it.
         test_runs_on_machine = platform_for_key.get("test-runs-on")
         if test_runs_on_machine:
-            gha_set_output({"test-runs-on": test_runs_on_machine})
+            print(f"  Found runner: '{test_runs_on_machine}'")
+            return test_runs_on_machine
+
+    return ""
+
+
+def main(target: str, platform: str):
+    runner_label = get_runner_label(target, platform)
+    if runner_label:
+        gha_set_output({"test-runs-on": runner_label})
 
 
 if __name__ == "__main__":
