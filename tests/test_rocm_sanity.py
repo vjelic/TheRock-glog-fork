402da171efcef3c901ea3a36de3edb7db9cdf079
geomin12 <geomin12@amd.com>
2025-08-07 16:58:47 -0700

    removing logs

--- tests/test_rocm_sanity.py
+++ tests/test_rocm_sanity.py
@@ -20,9 +20,6 @@ def is_windows():
 
 def run_command(command, cwd=None):
     process = subprocess.run(command, capture_output=True, cwd=cwd, shell=is_windows())
-    logger.info(str(process))
-    logger.info(str(process.stdout))
-    logger.info(str(process.stderr))
     if process.returncode != 0:
         raise Exception(str(process.stderr))
     return process
